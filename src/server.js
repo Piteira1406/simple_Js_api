9176e41bd95f5bbe048d2e9c0ff7251fed1a4e3d
Jõao Piteira <joaoduarte1406@gmail.com>
2025-04-06 16:00:04 +0100

    simple change

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app= express();
 app.use(cors());
 app.use(express.json());
 
-const Port = process.env.PORT || 5000;
+const Port = process.env.PORT || '(PUT YOUR PORT HERE)';
 
 app.get('/', (req, res) => {
     res.send('Hello from server!');
